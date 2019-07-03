ba2c46b3d858fd3646976c345a15e852fa4c4e3d
Daniel Manrique Lucas <dmanluc91@gmail.com>
2019-07-03 15:06:38 +0200

    Add initial splash activity

---
+++ app/src/main/kotlin/com/dmanluc/cabifymarket/presentation/feature/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.dmanluc.cabifymarket.presentation.feature.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dmanluc.cabifymarket.presentation.MarketActivity
+
+/**
+ * @author   Daniel Manrique Lucas <dmanluc91@gmail.com>
+ * @version  1
+ * @since    2019-07-02.
+ */
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MarketActivity::class.java))
+        finish()
+    }
+
+}
